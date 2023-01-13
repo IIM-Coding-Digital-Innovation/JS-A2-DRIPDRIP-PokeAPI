2efef633857ac7f574944ab253006ab0debc8a3d
Felix/failaxite <failaxite.don@gmail.com>
2023-01-13 01:16:08 +0100

    :art::lipstick: Adding a style card for pokemon

--- api.js
+++ api.js
@@ -14,6 +14,10 @@ function filterPokemon() {
                     return fetch(pokemon.url)
                         .then(response => response.json())
                         .then(pokemonData => {
+                            // Créer la div conteneur pour chaque élément <li>
+                            const pokemonContainer = document.createElement('div');
+                            pokemonContainer.classList.add('pokemon-card');
+                            // Créer l'élément <li> pour chaque pokemon
                             const pokemonElement = document.createElement('li');
                             const nameElement = document.createElement('p');
                             const imageElement = document.createElement('img');
@@ -22,58 +26,64 @@ function filterPokemon() {
                             imageElement.alt = pokemonData.name;
                             pokemonElement.appendChild(nameElement);
                             pokemonElement.appendChild(imageElement);
-                            return pokemonElement;
+                            // Ajouter l'élément <li> à la div conteneur
+                            pokemonContainer.appendChild(pokemonElement);
+                            return pokemonContainer;
                         });
                 });
                 // Attendre que toutes les requêtes soient terminées
-                Promise.all(pokemonList).then(pokemonElements => {
+                Promise.all(pokemonList).then(pokemonContainers => {
                     // Ajouter la liste des pokemons à l'élement "pokemon-list"
                     const pokemonListElement = document.getElementById('pokemon-list');
                     // Supprimer les enfants actuels
                     while (pokemonListElement.firstChild) {
                         pokemonListElement.removeChild(pokemonListElement.firstChild);
                     }
-                    pokemonElements.forEach(pokemonElement => {
-                        pokemonListElement.appendChild(pokemonElement);
+                    pokemonContainers.forEach(pokemonContainer => {
+                        pokemonListElement.appendChild(pokemonContainer);
                     });
                 });
             });
-    } else {
-    // Utiliser l'API PokeAPI pour récupérer les pokemons du type sélectionné
-    fetch(`https://pokeapi.co/api/v2/type/${selectedType}`)
-        .then(response => response.json())
-        .then(data => {
-            // Récupérer les informations de Pokémon de la réponse
-            const pokemons = data.pokemon;
-            const pokemonList = pokemons.map(pokemon => {
-                // Récupérer les informations de chaque pokemon
-                return fetch(pokemon.pokemon.url)
-                    .then(response => response.json())
-                    .then(pokemonData => {
-                        const pokemonElement = document.createElement('li');
-                        const nameElement = document.createElement('p');
-                        const imageElement = document.createElement('img');
-                        nameElement.textContent = pokemonData.name;
-                        imageElement.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonData.id}.png`;
-                        imageElement.alt = pokemonData.name;
-                        pokemonElement.appendChild(nameElement);
-                        pokemonElement.appendChild(imageElement);
-                        return pokemonElement;
+        } else {
+            // Utiliser l'API PokeAPI pour récupérer les pokemons du type sélectionné
+            fetch(`https://pokeapi.co/api/v2/type/${selectedType}`)
+                .then(response => response.json())
+                .then(data => {
+                    const pokemons = data.pokemon;
+                    pokemonList = pokemons.map(pokemon => {
+                        return fetch(pokemon.pokemon.url)
+                            .then(response => response.json())
+                            .then(pokemonData => {
+                                // Créer la div conteneur pour chaque élément <li>
+                                const pokemonContainer = document.createElement('div');
+                                pokemonContainer.classList.add('pokemon-card');
+                                // Créer l'élément <li> pour chaque pokemon
+                                const pokemonElement = document.createElement('li');
+                                const nameElement = document.createElement('p');
+                                const imageElement = document.createElement('img');
+                                nameElement.textContent = pokemonData.name;
+                                imageElement.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonData.id}.png`;
+                                imageElement.alt = pokemonData.name;
+                                pokemonElement.appendChild(nameElement);
+                                pokemonElement.appendChild(imageElement);
+                                // Ajouter l'élément <li> à la div conteneur
+                                pokemonContainer.appendChild(pokemonElement);
+                                return pokemonContainer;
+                            });
                     });
-            });
-            // Attendre que toutes les requêtes soient terminées
-            Promise.all(pokemonList).then(pokemonElements => {
-                // Ajouter la liste des pokemons à l'élement "pokemon-list"
-                const pokemonListElement = document.getElementById('pokemon-list');
-                // Supprimer les enfants actuels
-                while (pokemonListElement.firstChild) {
-                    pokemonListElement.removeChild(pokemonListElement.firstChild);
-                }
-                pokemonElements.forEach(pokemonElement => {
-                    pokemonListElement.appendChild(pokemonElement);
+                    // Attendre que toutes les requêtes soient terminées
+                    Promise.all(pokemonList).then(pokemonContainers => {
+                        // Ajouter la liste des pokemons à l'élement "pokemon-list"
+                        const pokemonListElement = document.getElementById('pokemon-list');
+                        // Supprimer les enfants actuels
+                        while (pokemonListElement.firstChild) {
+                            pokemonListElement.removeChild(pokemonListElement.firstChild);
+                        }
+                        pokemonContainers.forEach(pokemonContainer => {
+                            pokemonListElement.appendChild(pokemonContainer);
+                        });
+                    });
                 });
-            });
-        });
     }
   }
 
